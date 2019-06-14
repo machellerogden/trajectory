97bbc5ddaf63d7e5bdf57e00f7d210702be7b342
Mac Heller-Ogden <mac@machellerogden.com>
2019-06-14 14:57:34 -0500

    broken wip

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 
 const { Schema } = require('./lib/schema');
 const { Scheduler, exec } = require('./lib/scheduler');
+const bus = require('./lib/bus');
 
 module.exports = { execute };
 
 async function execute(definition, input) {
+    bus.start();
     const { spec:queue } = await Schema.validate(definition);
-    const results = [ input, ...(await exec(queue, input)) ];
+    const results = [ input, ...(await exec(queue, input, bus)) ];
     return results;
 }

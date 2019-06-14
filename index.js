116513982314dec9c990f2798f0c0025d0bd5fb1
Mac Heller-Ogden <mac@machellerogden.com>
2019-06-13 23:03:41 -0500

    some progress

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
 
-module.exports = () => {};
+
+const { Schema } = require('./lib/schema');
+const { Scheduler, exec } = require('./lib/scheduler');
+
+module.exports = { execute };
+
+async function execute(definition, input) {
+    const { spec:queue } = await Schema.validate(definition);
+    const results = [ input, ...(await exec(queue, input)) ];
+    return results;
+}

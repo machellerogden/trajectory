1d9cc48a0bc26e710654d314da1dd88e99ad62ed
Mac Heller-Ogden <mac@machellerogden.com>
2019-06-16 07:54:56 -0500

    moving in the right direction

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ const { clone } = require('mediary');
 
 const { schema } = require('./lib/schema');
 const { sleep } = require('./lib/util');
-const defaultReporter = require('./lib/reporter');
+const { isTerminal } = require('./lib/helpers');
+const { defaultReporter, defaultReporterOptions } = require('./lib/defaults');
 
 class Trajectory extends EventEmitter {
 
     constructor({
         debug = false,
-        reporter = defaultReporter
+        reporter = defaultReporter,
+        reporterOptions = defaultReporterOptions
     } = {}) {
         super();
         this.reporter = reporter;
-        this.indent = 0;
-        this.indentCols = 4;
+        this.indent = reporterOptions.indent || defaultReporterOptions.indent;
+        this.indentCols = reporterOptions.indentCols || defaultReporterOptions.indentCols;
         this.on('event', ({ messageType, name, data }) => {
             this.reporter[messageType]({ name, data, indent: this.indent });
         });
@@ -26,8 +28,12 @@ class Trajectory extends EventEmitter {
     async execute(definition, input) {
         const { spec:queue } = await schema.validate(definition);
         const results = [];
-        for await (const result of this.schedule(queue, input)) {
-            results.push(result);
+        try {
+            for await (const result of this.schedule(queue, input)) {
+                results.push(result);
+            }
+        } catch (e) {
+            console.log('boom');
         }
         return results;
     }
@@ -38,6 +44,8 @@ class Trajectory extends EventEmitter {
 
         async function* loop(state, start) {
             let name; 
+
+            if (state == null || state[start] == null) throw new Error(`Unable to resolve state "${start}".`);
 
             if (start) {
                 name = start;
@@ -57,73 +65,56 @@ class Trajectory extends EventEmitter {
                 yield { name, data };
             }
 
+            const handlers = {
+                task: async function * taskHandler() {
+                        io = await state.fn(await io)
+                        yield* output(io);
+                        emit('succeed', io);
+                },
+                pass: async function * passHandler() {
+                        if (state.result != null) {
+                            io = await state.result;
+                        }
+                        yield* output(io);
+                        emit('succeed', io);
+                },
+                wait: async function * waitHandler() {
+                        if (state.seconds) {
+                            await sleep(state.seconds);
+                        } else if (state.secondsPath) {
+                            await sleep(get(io, state.secondsPath));
+                        }
+                        yield* output(io);
+                        emit('succeed', io);
+                },
+                succeed: async function * succeedHandler() {
+                        yield* output(io);
+                        emit('succeed', io);
+                },
+                fail: async function * failHandler() {
+                        const err = { name, io };
+                        if (state.error) err.error = state.error;
+                        if (state.cause) err.cause = state.cause;
+                        yield* output(err);
+                        emit('fail', err);
+                },
+                parallel: async function * parallelHandler() {
+                        $this.indent += $this.indentCols;
+                        io = await Promise.all(state.branches.map(branch => $this.executeBranch(branch, clone(io))));
+                        $this.indent -= $this.indentCols;
+                        yield* output(io);
+                        emit('succeed', io);
+                },
+                choice: async function * choiceHandler() {
+                    // TODO
+                }
+            };
+
             while (true) {
                 emit('start');
-                try {
-                    switch (state.type) {
-                        case 'task': {
-                            io = await state.fn(await io)
-                            yield* output(io);
-                            emit('succeed', io);
-                            if (state.end) return;
-                            next();
-                            break;
-                        }
-                        case 'pass':
-                            if (state.result != null) {
-                                io = await state.result;
-                            }
-                            yield* output(io);
-                            emit('succeed', io);
-                            next();
-                            break;
-                        case 'wait':
-                            if (state.seconds) {
-                                await sleep(state.seconds);
-                            } else if (state.secondsPath) {
-                                await sleep(get(io, state.secondsPath));
-                            }
-                            yield* output(io);
-                            emit('succeed', io);
-                            next();
-                            break;
-                        case 'succeed':
-                            yield* output(io);
-                            emit('succeed', io);
-                            return;
-                        case 'fail': {
-                            const err = { name, io };
-                            if (state.error) err.error = state.error;
-                            if (state.cause) err.cause = state.cause;
-                            yield* output(err);
-                            emit('fail', err);
-                            return;
-                        }
-                        case 'parallel':
-                            $this.indent += $this.indentCols;
-                            io = await Promise.all(state.branches.map(branch => $this.executeBranch(branch, clone(io))));
-                            $this.indent -= $this.indentCols;
-                            yield* output(io);
-                            emit('succeed', io);
-                            if (state.end) return;
-                            next();
-                            break;
-                        case 'choice':
-                            // TODO
-                            break;
-                        default: {
-                            const err = { message: `\`type\` of "${state.type}" for "${name}" not found.` };
-                            yield* output(err);
-                            emit('fail', err);
-                            break;
-                        }
-
-                    }
-                } catch (e) {
-                    yield* output(e);
-                    emit('fail', e);
-                    return;
-                }
+                yield* handlers[state.type]();
+                if (isTerminal(state)) return;
+                next();
             }
         }
         yield * await loop(states, startAt);

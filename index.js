a522464278623ec57a34f56cb1f9bafe0f725530
Mac Heller-Ogden <mac@machellerogden.com>
2019-06-18 06:57:39 -0500

    starting to test io control flow

--- index.js
+++ index.js
@@ -70,112 +70,104 @@ class Trajectory extends EventEmitter {
         const { startAt, states } = queue;
         const $this = this;
 
-        async function* loop(state, start) {
-            let name; 
+        if (states == null || states[startAt] == null) throw new Error(`Unable to resolve state "${startAt}".`);
 
-            if (state == null || state[start] == null) throw new Error(`Unable to resolve state "${start}".`);
+        let name = startAt;
+        let state = states[startAt];
 
-            if (start) {
-                name = start;
-                state = state[start];
-            }
-
-            function next() {
-                name = state.next;
-                state = states[state.next];
-            }
-
-            function applyParameters(data) {
-                if (state.parameters == null) return data;
-                function recur(value) {
-                    if (value == null || typeof value !== 'object') return value;
-                    return Array.isArray(value)
-                        ? value.reduce((acc, v, i) => applyP(acc, i, v, data, recur), [])
-                        : Object.entries(value).reduce((acc, [ k, v ]) => applyP(acc, k, v, data, recur), {});
-                }
-                return recur(state.parameters);
-            }
-
-            function getInput(data) {
-                if (state.inputPath == null) return data;
-                return JSONPath.query(data, state.inputPath)
-            }
-
-            async function getOutput(data) {
-                if (state.outputPath == null) return data;
-                return JSONPath.query(await data, state.outputPath);
-            }
-
-            async function setResult(value) {
-                if (state.result) return state.result;
-                if (state.resultPath == null) return await value;
-                return set(clone(io), state.resultPath, await value);
-            }
-
-            async function * output(type, o) {
-                const data = await o;
-                yield { name, data };
-                $this.emit('event', { type, name, data });
-                io = clone(data); // set data for next loop
-            }
-
-            const handlers = {
-                async * task() {
-                    yield* await output('succeed', getOutput(setResult(state.fn(applyParameters(getInput(io))))));
-                },
-                async * pass() {
-                    yield* await output('succeed', getOutput(setResult(applyParameters(getInput(io)))));
-                },
-                async * wait() {
-                    let out = getInput(io);
-                    if (state.seconds != null) {
-                        await sleep(state.seconds);
-                    } else if (state.secondsPath != null) {
-                        const seconds = JSONPath.query(out, state.secondsPath);
-                        await sleep(seconds);
-                    }
-                    yield* output('succeed', out);
-                },
-                async * succeed() {
-                    let out = io;
-                    yield* output('succeed', out);
-                },
-                async * fail() {
-                    let out = io;
-                    const err = { name, out };
-                    const errMsg = [];
-                    if (state.error) {
-                        err.error = state.error;
-                        errMsg.push(err.error);
-                    }
-                    if (state.cause) {
-                        err.cause = state.cause;
-                        errMsg.push(err.cause);
-                    }
-                    yield* output('fail', err);
-                    throw new Error(errMsg.join(': '));
-                },
-                async * parallel() {
-                    $this.depth++;
-                    let out = await Promise.all(state.branches.map(branch => $this.executeQueue(branch, io)));
-                    $this.depth--;
-                    io = out;
-                    yield* output('succeed', out);
-                },
-                async * choice() {
-                    // TODO
-                }
-            };
-
-            while (true) {
-                $this.emit('event', { type: 'start', name });
-                yield* handlers[state.type]();
-                if (isEnd(state)) return;
-                next();
-            }
+        function next() {
+            name = state.next;
+            state = states[state.next];
         }
 
-        yield * await loop(states, startAt);
+        function applyParameters(data) {
+            if (state.parameters == null) return data;
+            function recur(value) {
+                if (value == null || typeof value !== 'object') return value;
+                return Array.isArray(value)
+                    ? value.reduce((acc, v, i) => applyP(acc, i, v, data, recur), [])
+                    : Object.entries(value).reduce((acc, [ k, v ]) => applyP(acc, k, v, data, recur), {});
+            }
+            return recur(state.parameters);
+        }
+
+        function getInput(data) {
+            if (state.inputPath == null) return data;
+            return JSONPath.query(data, state.inputPath).shift();
+        }
+
+        async function getOutput(data) {
+            if (state.outputPath == null) return data;
+            return JSONPath.query(await data, state.outputPath).shift();
+        }
+
+        async function setResult(value) {
+            if (state.result) return state.result;
+            if (state.resultPath == null) return await value;
+            return set(clone(io), state.resultPath, await value);
+        }
+
+        async function * output(type, o) {
+            const data = await o;
+            yield { name, data };
+            $this.emit('event', { type, name, data });
+            io = clone(data); // set data for next loop
+        }
+
+        const handlers = {
+            async * task() {
+                yield* await output('succeed', getOutput(setResult(state.fn(applyParameters(getInput(io))))));
+            },
+            async * pass() {
+                yield* await output('succeed', getOutput(setResult(applyParameters(getInput(io)))));
+            },
+            async * wait() {
+                let out = getInput(io);
+                if (state.seconds != null) {
+                    await sleep(state.seconds);
+                } else if (state.secondsPath != null) {
+                    const seconds = JSONPath.query(out, state.secondsPath);
+                    await sleep(seconds);
+                }
+                yield* output('succeed', out);
+            },
+            async * succeed() {
+                let out = io;
+                yield* output('succeed', out);
+            },
+            async * fail() {
+                let out = io;
+                const err = { name, out };
+                const errMsg = [];
+                if (state.error) {
+                    err.error = state.error;
+                    errMsg.push(err.error);
+                }
+                if (state.cause) {
+                    err.cause = state.cause;
+                    errMsg.push(err.cause);
+                }
+                yield* output('fail', err);
+                throw new Error(errMsg.join(': '));
+            },
+            async * parallel() {
+                $this.depth++;
+                let out = await Promise.all(state.branches.map(branch => $this.executeQueue(branch, io)));
+                $this.depth--;
+                io = out;
+                yield* output('succeed', out);
+            },
+            async * choice() {
+                // TODO
+            }
+        };
+
+        while (true) {
+            $this.emit('event', { type: 'start', name });
+            yield* handlers[state.type]();
+            if (isEnd(state)) return;
+            next();
+        }
     }
 }
 
@@ -183,7 +175,7 @@ module.exports = { Trajectory };
 
 function applyP(d, a, k, v, fn) {
     if (k.endsWith('.$')) {
-        acc[k.slice(0, -2)] = JSONPath.query(d, v);
+        acc[k.slice(0, -2)] = JSONPath.query(d, v).shift();
     } else {
         acc[k] = fn(v);
     }
--- test/index.spec.js
+++ test/index.spec.js
@@ -83,6 +83,153 @@ test('should thread io through states', async t => {
     t.deepEqual(results, [ {}, { a: 'a' }, { b: 'b' }, { c: 'c' } ]);
 });
 
+test('should thread io through states - resultPath', async t => {
+    t.plan(4);
+    const a = t.context.sandbox.fake.returns({ a: 'a' });
+    const b = t.context.sandbox.fake.returns({ b: 'b' });
+    const c = t.context.sandbox.fake.returns({ c: 'c' });
+    const definition = {
+        kind: 'queue',
+        version: '1.0.0',
+        spec: {
+            startAt: 'a',
+            states: {
+                a: {
+                    type: 'task',
+                    fn: a,
+                    resultPath: 'apath',
+                    next: 'b'
+                },
+                c: {
+                    type: 'task',
+                    fn: c,
+                    resultPath: 'cpath',
+                    end: true
+                },
+                b: {
+                    type: 'task',
+                    fn: b,
+                    resultPath: 'bpath',
+                    next: 'c'
+                }
+            }
+        }
+    };
+    const trajectory = new Trajectory(testOptions);
+    const results = await trajectory.execute(definition);
+    t.assert(a.calledWith({}));
+    t.assert(b.calledWith({
+        'apath': {
+            a: 'a'
+        }
+    }));
+    t.assert(c.calledWith({
+        'apath': {
+            a: 'a'
+        },
+        'bpath': {
+            b: 'b'
+        }
+    }));
+    t.deepEqual(results, [
+        {},
+        {
+            'apath': {
+                a: 'a'
+            }
+        },
+        {
+            'apath': {
+                a: 'a'
+            },
+            'bpath': {
+                b: 'b'
+            }
+        }, {
+            'apath': {
+                a: 'a'
+            },
+            'bpath': {
+                b: 'b'
+            },
+            'cpath': {
+                c: 'c'
+            }
+        }
+    ]);
+});
+
+test('should thread io through states - resultPath + inputPath', async t => {
+    t.plan(4);
+    const a = t.context.sandbox.fake.returns({ a: 'a' });
+    const b = t.context.sandbox.fake.returns({ b: 'b' });
+    const c = t.context.sandbox.fake.returns({ c: 'c' });
+    const definition = {
+        kind: 'queue',
+        version: '1.0.0',
+        spec: {
+            startAt: 'a',
+            states: {
+                a: {
+                    type: 'task',
+                    fn: a,
+                    resultPath: 'apath',
+                    next: 'b'
+                },
+                c: {
+                    type: 'task',
+                    fn: c,
+                    inputPath: '$.bpath',
+                    resultPath: 'cpath',
+                    end: true
+                },
+                b: {
+                    type: 'task',
+                    fn: b,
+                    inputPath: '$.apath',
+                    resultPath: 'bpath',
+                    next: 'c'
+                }
+            }
+        }
+    };
+    const trajectory = new Trajectory(testOptions);
+    const results = await trajectory.execute(definition);
+    t.assert(a.calledWith({}));
+    t.assert(b.calledWith({
+        a: 'a'
+    }));
+    t.assert(c.calledWith({
+        b: 'b'
+    }));
+    t.deepEqual(results, [
+        {},
+        {
+            'apath': {
+                a: 'a'
+            }
+        },
+        {
+            'apath': {
+                a: 'a'
+            },
+            'bpath': {
+                b: 'b'
+            }
+        }, {
+            'apath': {
+                a: 'a'
+            },
+            'bpath': {
+                b: 'b'
+            },
+            'cpath': {
+                c: 'c'
+            }
+        }
+    ]);
+});
+
 test('should handle parallel executions', async t => {
     t.plan(3);
     const b = t.context.sandbox.fake.returns({ b: 'b' });

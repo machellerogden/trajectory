2e24ed17a78ae4052739597e99308d20c7cc4fe6
Mac Heller-Ogden <mac@machellerogden.com>
2019-06-23 22:37:02 -0500

    async cancellation sandbox

--- generator-sandbox.js
+++ generator-sandbox.js
@@ -1,28 +1,55 @@
-async function * subGen() {
-    return yield await 'What is your favorite color?';
+//async function * subGen() {
+    //return yield await 'What is your favorite color?';
+//}
+
+//async function * gen() {
+    //while (true) {
+        //const reply = yield* await subGen();
+        //console.log(reply);
+        //if (reply !== 'yellow') return await 'Wrong!'
+        //return await 'You may pass.';
+    //}
+//}
+
+//(async () => {
+    //const iter = gen();
+    //const q = (await iter.next()).value;
+    //console.log(q);
+    //const a = (await iter.next('blue')).value;
+    //console.log(a);
+//})();
+
+//(async () => {
+    //const iter = gen();
+    //const q = (await iter.next()).value;
+    //console.log(q);
+    //const a = (await iter.next('yellow')).value;
+    //console.log(a);
+//})();
+
+const { Cancel } = require('./lib/cancel');
+
+function sleep(ms, cancelled) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve('success'), ms);
+        cancelled.then(() => reject(new Error('cancelled')));
+    });
 }
 
-async function * gen() {
-    while (true) {
-        const reply = yield* await subGen();
-        console.log(reply);
-        if (reply !== 'yellow') return await 'Wrong!'
-        return await 'You may pass.';
+(async () => {
+
+    const c = new Cancel();
+
+    async function* loop() {
+        while (true) {
+            yield await c.withCancel(cancelled => sleep(1000, cancelled));
+        }
     }
-}
 
-(async () => {
-    const iter = gen();
-    const q = (await iter.next()).value;
-    console.log(q);
-    const a = (await iter.next('blue')).value;
-    console.log(a);
-})();
+    setTimeout(() => c.cancelAll(), 5000);
 
-(async () => {
-    const iter = gen();
-    const q = (await iter.next()).value;
-    console.log(q);
-    const a = (await iter.next('yellow')).value;
-    console.log(a);
+    for await (const result of loop()) {
+        console.log(result);
+    }
+
 })();
